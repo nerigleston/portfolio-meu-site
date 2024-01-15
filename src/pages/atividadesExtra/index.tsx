d9498d394df04f1c4d24efc40275315d67d8a74d
Neri Filho <nerifilho1256@gmail.com>
2024-01-15 18:54:48 -0300

    atividades

--- src/pages/atividadesExtra/index.tsx
+++ src/pages/atividadesExtra/index.tsx
@@ -5,11 +5,11 @@ export default function Atividades() {
         <div className="mb-8">
           <h1 className="text-3xl font-bold mb-4">Atividade Extracurricular</h1>
           <p className="text-lg flex items-center justify-center text-center h-[50vh] ">
-            Estou tendo o privilégio de participar de um programa de imersão
+            Tive o privilégio de participar de um programa de imersão
             chamado “Fábrica de Software” realizado na UNIPÊ, que consiste em um
             programa no qual as empresas que, necessitam de um software, solicitam
             um projeto aos alunos e os mesmos formam equipes para a entrega de um
-            software. Essa experiência está sendo gratificante, enriquecedora,
+            software. Foi uma experiência gratificante, enriquecedora,
             proporcionando aprendizado prático e amplo em desenvolvimento de software.
           </p>
         </div>
